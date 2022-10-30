43ef0d61dcbbe4b7e8cbf0f95b28297f45b42b1f
Rohit Nimkar <nehalnimkar@gmail.com>
2022-10-30 13:29:30 +0530

    📦️ add external libraries

---
+++ 07-external-libraries/lib/message/Message.cpp
@@ -0,0 +1,16 @@
+#include "Message.hpp"
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+std::ostream &Message::printMessage(std::ostream &os) {
+  os << "This is my very nice message: " << message_ <<  std::endl;
+  return os;
+}
+
+
+Message::~Message()
+{
+}
---
+++ 07-external-libraries/src/main.cpp
@@ -0,0 +1,15 @@
+#include "Message.hpp"
+#include <iostream>
+
+
+int main(){
+
+    Message msg("Hello world");
+
+
+    std::cout << msg;
+
+
+    return 0;
+}
+
